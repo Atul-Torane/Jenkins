bfde1263667db72255bfa923fe56304b96c39a61
Atul Torane <55852411+Atul-Torane@users.noreply.github.com>
2024-06-23 15:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,3 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return 'Hello World'
-
-# main driver function
-if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
